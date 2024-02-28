21d6887216687ac93c1dc45b11a8afd623b049f9
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-02-28 16:03:10 +0100

    Task5

---
+++ g49-java-exercises/src/main/java/com/exercises/Task5.java
@@ -0,0 +1,12 @@
+package com.exercises;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void solve() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your username: ");
+        String userName = scanner.next();
+        System.out.println("Hello " + userName);
+    }
+}
