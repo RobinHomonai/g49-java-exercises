46fe39465cc068735dff85e0d542b92b0e2f132d
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-02-28 16:02:10 +0100

    Task3

---
+++ g49-java-exercises/src/main/java/com/exercises/Task3.java
@@ -0,0 +1,14 @@
+package com.exercises;
+
+public class Task3 {
+    public static void solve() {
+        int sum = 45 + 11;
+        int multiplication = 12 * 4;
+        int division = 24 / 6;
+        int subtraction = 55 - 12;
+        System.out.println("45 + 11 = " + sum);
+        System.out.println("12 * 4 = " + multiplication);
+        System.out.println("24 / 6 = " + division);
+        System.out.println("55 - 12 = " + subtraction);
+    }
+}
