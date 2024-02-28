7137ab410092b0e5cf307dadb7796c4d3d7e1d7a
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-02-28 15:58:56 +0100

    Initial commit

---
+++ g49-java-exercises/src/main/java/com/exercises/App.java
@@ -0,0 +1,27 @@
+package com.exercises;
+
+/**
+ * sout + tab = System.out.println
+ * psvm + tab = public static void main
+ * ctrl+alt+l = re-factor code
+ */
+public class App {
+    public static void main(String[] args) {
+//        System.out.println("----------------------");
+//        Task1.solve();
+//        System.out.println("----------------------");
+//        Task2.solve();
+//        System.out.println("----------------------");
+//        Task3.solve();
+//        System.out.println("----------------------");
+//        Task4.solve();
+//        System.out.println("----------------------");
+//        Task5.solve();
+//        System.out.println("----------------------");
+//        Task6.solve();
+//        System.out.println("----------------------");
+//        Task7.solve();
+//        System.out.println("----------------------");
+        Task8.solve();
+    }
+}
---
+++ g49-java-exercises/src/main/java/com/exercises/Task1.java
@@ -0,0 +1,9 @@
+package com.exercises;
+
+public class Task1 {
+    public static void solve() {
+        String myName = "Robin";
+        System.out.println("Hello");
+        System.out.println(myName + "!");
+    }
+}
