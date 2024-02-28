5380e505e0aece2595377abc6336f95df33a4c8d
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-02-28 16:02:41 +0100

    Task4

---
+++ g49-java-exercises/src/main/java/com/exercises/Task4.java
@@ -0,0 +1,11 @@
+package com.exercises;
+
+public class Task4 {
+    public static void solve() {
+        int num1 = 23;
+        int num2 = 11;
+        int num3 = 77;
+        int sum = (num1 + num2 + num3) / 3;
+        System.out.println("The average number of (23 + 11 + 77) / 3 = " + sum);
+    }
+}
